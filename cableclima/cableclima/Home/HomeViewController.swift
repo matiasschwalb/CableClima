d5b71f392b357d33fd69bee7fbb9c50e1ef7d5c5
matiasschwalb <maschwalb@gmail.com>
2019-09-10 20:16:13 -0300

    added some assets and deleted storyboard

---
+++ cableclima/cableclima/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  cableclima
+//
+//  Created by Matias David Schwalb on 10/09/2019.
+//  Copyright © 2019 Matias David Schwalb. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    let viewModel: HomeViewModel = HomeViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ cableclima/cableclima/Home/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  cableclima
+//
+//  Created by Matias David Schwalb on 10/09/2019.
+//  Copyright © 2019 Matias David Schwalb. All rights reserved.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+}
