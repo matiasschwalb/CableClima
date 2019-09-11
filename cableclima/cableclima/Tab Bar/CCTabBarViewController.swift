8419eb683797cd1177b7647027a11287e849f1b1
matiasschwalb <maschwalb@gmail.com>
2019-09-11 20:29:04 -0300

    added some files

--- cableclima/cableclima/Tab Bar/CCTabBarViewController.swift
+++ cableclima/cableclima/Tab Bar/CCTabBarViewController.swift
@@ -10,22 +10,41 @@ import UIKit
 
 class CCTabBarController: UITabBarController {
     
+    override func viewWillAppear(_ animated: Bool) {
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 17)], for: .selected)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font:UIFont.systemFont(ofSize: 17)], for: .normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tabBar.barTintColor = UIColor.ccRed
+        tabBar.tintColor = UIColor.white
+        tabBar.unselectedItemTintColor = UIColor.lightGray
+        
+
         
         let home = HomeViewController()
         home.tabBarItem = UITabBarItem()
         home.tabBarItem.title = "tab_bar.home.title".localized()
-//        home.tabBarItem.image =
+        let fonts = UIFont.familyNames
+        for font in fonts {
+            print(font)
+        }
+//        home.tabBarItem.setTitleTextAttributes([NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 17)], for: .selected)
+//        home.tabBarItem.setTitleTextAttributes([NSAttributedString.Key.font:UIFont.systemFont(ofSize: 17)], for: .normal)
+        home.tabBarItem.image = UIImage(named: "weather")
+        
         home.tabBarItem.tag = 0
+        
         
         let forecast = CCViewController() // This should be changed to a specific controller
         forecast.tabBarItem = UITabBarItem()
         forecast.tabBarItem.title = "tab_bar.forecast.title".localized()
-//        forecast.tabBarItem.image =
+        forecast.tabBarItem.image = UIImage(named: "forecast")
         forecast.tabBarItem.tag = 1
+//        forecast.tabBarItem.setTitleTextAttributes([NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 17)], for: .selected)
+        forecast.tabBarItem.setTitleTextAttributes([NSAttributedString.Key.font:UIFont.systemFont(ofSize: 17)], for: .normal)
         
         viewControllers = [home, forecast]
     }
